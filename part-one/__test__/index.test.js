2423a9d41894e3d631529a4f55aa07c8ee4ddc60
unknown <songal4546@gmail.com>
2020-03-20 11:59:54 +0900

    functional, functional-shared completed

--- part-one/__test__/index.test.js
+++ part-one/__test__/index.test.js
@@ -1,23 +1,23 @@
-const runner = require("./testRunner");
+const runner = require('./testRunner');
 
 // These are test case for each of stlyes.
 // If you need to run a test, comment out the code block
 
 // Test for functional style
 
-runner(
-  "functional",
-  require("../src/functional/stack"),
-  require("../src/functional/queue")
-);
+// runner(
+//   "functional",
+//   require("../src/functional/stack"),
+//   require("../src/functional/queue")
+// );
 
 // Test for Functional-shared style
 
-// runner(
-//   "functional-shared",
-//   require("../src/functional-shared/stack"),
-//   require("../src/functional-shared/queue")
-// );
+runner(
+  'functional-shared',
+  require('../src/functional-shared/stack'),
+  require('../src/functional-shared/queue')
+);
 
 // Test for Prototypal style
 
--- part-one/src/functional/queue.js
+++ part-one/src/functional/queue.js
@@ -4,18 +4,36 @@ const Queue = function() {
   // Use an object with numeric keys to store values
   const storage = {};
 
+  let count = 0;
+
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {};
+  someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
+  };
 
-  someInstance.dequeue = function() {};
+  someInstance.dequeue = function() {
+    if (count < 1) {
+      return;
+    }
+    var pullData = storage[0];
 
-  someInstance.size = function() {};
+    for (let i = 1; i < count; i++) {
+      storage[i - 1] = storage[i];
+    }
+    count--;
+    return pullData;
+  };
+
+  someInstance.size = function() {
+    return count;
+  };
 
   return someInstance;
 };
 
-if (typeof module === "object" && typeof module.exports === "object") {
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Queue
   };
