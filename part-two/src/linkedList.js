4451cf93bc6aabcb96d2f9bd4b460bd24f409af9
unknown <songal4546@gmail.com>
2020-03-23 16:21:31 +0900

    ongoing

--- part-two/src/linkedList.js
+++ part-two/src/linkedList.js
@@ -3,11 +3,42 @@ const LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {};
+  list.addToTail = function(value) {
+    let newNode = new Node(value);
 
-  list.removeHead = function() {};
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail = newNode;
+      this.tail.next = newNode;
+    }
+    return newNode;
+  };
 
-  list.contains = function(target) {};
+  list.removeHead = function() {
+    var newHead = this.head.next;
+
+    var removeValue = this.head;
+    removeValue.next = null;
+
+    this.head = newHead;
+
+    return removeValue.value;
+  };
+
+  list.contains = function(target) {
+    var current = this.head;
+
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+    return false;
+  };
 
   return list;
 };
