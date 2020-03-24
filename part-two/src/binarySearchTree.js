0668de627975253a967324b66dc23d0b9df90891
NakSeo <nakseono@gmail.com>
2020-03-24 11:12:35 +0900

    BST doing

--- part-two/src/binarySearchTree.js
+++ part-two/src/binarySearchTree.js
@@ -1,4 +1,71 @@
-const BinarySearchTree = function (value) {};
+const BinarySearchTree = function (value) {
+  const newTree = Object.create(newTreeMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
+  return newTree;
+};
+
+const newTreeMethods = {};
+
+newTreeMethods.insert = function (value) {
+  const node = new BinarySearchTree(value);
+
+  const nodeAdd = function (target) {
+    if (target.value <= node.value) {
+      if (target.left === null) {
+        target.left = node;
+      } else {
+        nodeAdd(target.left);
+      }
+    } else if ((target.right = null)) {
+      target.right = node;
+    } else {
+      nodeAdd(target.right);
+    }
+  };
+
+  nodeAdd(this);
+};
+
+newTreeMethods.contains = function(target){
+  const container = function(some){
+    if(some.value >= target){
+      if(some.left.value === target){
+        return true;
+      } else {
+        container(some.left.value);
+      }
+    } else {
+      if(some.right.value === target){
+        return true;
+      } else {
+        container(some.right.value);
+      }
+    } 
+  }
+  container(this);
+  return false;
+};
+
+
+newTreeMethods.depthFirstLog = fucntion(func){
+  function myFunc(some) {
+    if(some.value !== null){
+      func(some.value);
+        if(some.left !== null){
+          myFunc(some.left);
+        } 
+        if(some.right !== null){
+          myFunc(some.right);
+        }
+    }
+  }
+  myFunc(this);
+};
+  
+
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- part-two/src/tree.js
+++ part-two/src/tree.js
@@ -3,17 +3,29 @@ const Tree = function (value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null; // fix me
-
+  newTree.children = []; // fix me
   return newTree;
 };
 
 const treeMethods = {};
 
-treeMethods.addChild = function (value) {};
+treeMethods.addChild = function (value) {
+  const tree = new Tree(value);
+  this.children.push(tree);
+};
 
-treeMethods.contains = function (target) {};
-
+treeMethods.contains = function (target) {
+  const container = function (children, target) {
+    for (let i = 0; i < children.length; i++) {
+      if (target === children[i].value) {
+        return true;
+      }
+      container(children[i].children, target);
+    }
+  };
+  container(this.childern, target);
+  return false;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
