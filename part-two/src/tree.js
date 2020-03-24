ee0b3e7b729db87c1e8120de80e4f76896f67305
unknown <songal4546@gmail.com>
2020-03-24 11:15:17 +0900

    tree

--- part-two/src/tree.js
+++ part-two/src/tree.js
@@ -1,18 +1,34 @@
 const Tree = function(value) {
-  const newTree = {};
+  const newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null; // fix me
+  newTree.children = []; // fix me
 
   return newTree;
 };
 
 const treeMethods = {};
 
-treeMethods.addChild = function(value) {};
+treeMethods.addChild = function(value) {
+  let tree = new Tree(value);
 
-treeMethods.contains = function(target) {};
+  this.children.push(tree);
+};
+
+treeMethods.contains = function(target) {
+  let container = function(children, target) {
+    for (let i = 0; i < children.lenght; i++) {
+      if (children[i].value === target) {
+        return true;
+      } else {
+        container(children[i].children, target);
+      }
+    }
+  };
+  container(this.children, target);
+  return false;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
