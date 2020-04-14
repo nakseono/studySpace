597f412b8dc9dec8dc4dfc24c4fdc498c80a2013
NakSeo <nakseono@gmail.com>
2020-04-14 14:52:06 +0900

    complete bare #1

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VideoListEntry from './VideoListEntry';
 
 // 실제 API를 쓰게 되면 이 fakeData는 더이상 import 하지 않아야 합니다.
 import { fakeData } from './__test__/fakeData';
-console.log(fakeData)
+console.log(fakeData);
 
-const VideoList = () => (
+
+class VideoList extends Component {
+  render() {
+    return (
+      <div className="video-list media">
+        {fakeData.map(data => {
+          <VideoListEntry id={data.id.videoId} />
+        })}
+      </div>
+    )
+  }
+}
+
+/* const VideoList = () => (
   <div className="video-list media">
     <VideoListEntry />
     <VideoListEntry />
@@ -17,6 +30,12 @@ const VideoList = () => (
     <VideoListEntry />
     <VideoListEntry />
   </div>
-);
+); */
 
 export default VideoList;
+
+/* const Fake = fakeData.map((id) => {
+  <li key={id.videoId}>
+    {id}
+  </li>}
+  ); */
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { YOUTUBE_API_KEY } from '../config/youtube';
 
-const title = 'hello world';
+// const title = 'hello world';
 console.log(App);
 console.log(YOUTUBE_API_KEY);
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app')
 );
