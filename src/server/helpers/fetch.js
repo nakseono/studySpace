6e43c246c70c83bcf4978acb03779f1769bd9385
hong <snh1003@naver.com>
2020-04-28 11:38:22 +0900

    helper 완성

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -2,6 +2,16 @@ const https = require('https');
 
 async function retrieveArticle(url) {
   // TODO: retrieve the html string from given url and return as promise
+    return new Promise((resolve, reject) => {
+        https.get(url, res => {
+            let data;
+            res.on('data',chunk => {
+                data += chunk
+            })
+                .on('end', () => resolve(data))
+                .on('error', err=> reject(err))
+        })
+    })
 }
 
 module.exports = {
