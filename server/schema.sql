cb8ac3f3f31d53cff85d53f1a43ca4a0217cd7f6
NakSeo <nakseono@gmail.com>
2020-05-04 11:02:05 +0900

    test

--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,27 @@
 DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 USE chat;
+
+CREATE TABLE `messages` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `text` varchar(255),
+  `roomname` varchar(255)
+);
+
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255)
+);
+
+CREATE TABLE `user_message` (
+  `user_id` int,
+  `message_id` int
+);
+
+ALTER TABLE `user_message` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `user_message` ADD FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`);
+
 /*
 CREATE TABLE messages (
   //  remove here and create your message table
