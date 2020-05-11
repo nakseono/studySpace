845fd5de9bf31f43c52ab54af5096c10061edc35
NakSeo <nakseono@gmail.com>
2020-05-11 17:34:44 +0900

    bare done

--- server/controller/user/info.js
+++ server/controller/user/info.js
@@ -4,15 +4,15 @@ module.exports = {
   get: (req, res) => {
     // TODO : 유저의 session을 이용하여, 데이터베이스에 있는 정보를 제공하도록 구현하세요.
     if(!req.session.user){
-      res.status(401).send('need user session')
+      return res.status(401).send('need user session')
     }
 
     users
-    .findOne({
-      where: {
-        email: req.session.user.email
-      }
-    })
-    .then(result => res.status(200).json(result))
+      .findOne({
+        where: {
+          email: req.session.user.email
+        }
+      })
+      .then(result => res.status(200).json(result))
   }
 };
